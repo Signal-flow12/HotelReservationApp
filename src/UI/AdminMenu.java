76e87e64bd5525b9c3158909309f37c7242f8e4e
Signal-flow12 <tcravenny@gmail.com>
2024-01-25 20:39:04 -0500

    Add room working

---
+++ src/UI/AdminMenu.java
@@ -0,0 +1,96 @@
+package UI;
+import api.AdminResource;
+import model.Customer;
+import model.IRoom;
+import model.Room;
+import model.RoomTypeEnum;
+
+import java.util.*;
+
+public class AdminMenu {
+
+    AdminResource adminResource = AdminResource.getInstance();
+    public boolean start() {
+        boolean keepRunning = true;
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (keepRunning) {
+                try {
+                    System.out.println( "ADMIN MENU");
+                    System.out.println("1. See all customers");
+                    System.out.println("2. See all rooms");
+                    System.out.println("3. See all reservations");
+                    System.out.println("4. Add a room");
+                    System.out.println("5. Exit");
+                    System.out.println("----------------------------------------------- \n " +
+                            "Please select a number for the menu option.");
+                    int selection = Integer.parseInt(scanner.nextLine());
+                    if (selection == 1) {
+                        if (adminResource != null) {
+                            System.out.println("All Customers:");
+                            Collection<Customer> customers = adminResource.getAllCustomers();
+                            for (Customer customer : customers) {
+                                System.out.println(customer);
+                            }
+                        } else {
+                            System.out.println("No customers");
+                        }
+
+                    }
+                    if (selection == 2 ) {
+                        System.out.println();
+                    }
+                    if (selection == 3) {
+                        System.out.println();
+                    }
+                    if (selection == 4) {
+                        System.out.println("Add a room");
+                        addRoom(scanner);
+                    }
+                    if (selection == 5) {
+                        MainMenu mainMenu = new MainMenu();
+                        mainMenu.start();
+                    }
+                } catch (Exception ex) {
+                    System.out.println("\nError - Invalid Input\n");
+                    ex.printStackTrace();
+                }
+            }
+
+        }
+        return false;
+    }
+    private static void addRoom (Scanner input) {
+        AdminResource adminResource = AdminResource.getInstance();
+        String addRoom = "y";
+
+        while (addRoom.equals("y")) {
+            RoomTypeEnum roomType = null;
+
+            System.out.println("Enter room number: ");
+            String roomNumber = input.nextLine();
+
+            System.out.println("Enter price per night: ");
+            Double price = input.nextDouble();
+
+            while (roomType == null) {
+                System.out.println("Enter room type: 1 - Single bed, 2 - Double bed");
+                int type = input.nextInt();
+
+                if (type == 1) {
+                    roomType = RoomTypeEnum.SINGLE;
+                } else if (type == 2) {
+                    roomType = RoomTypeEnum.DOUBLE;
+                } else {
+                    System.out.println("Invalid input try again");
+                }
+            }
+            IRoom room = new Room(roomNumber, price, roomType);
+            List<IRoom> rooms = new ArrayList<>();
+            rooms.add(room);
+
+            System.out.println("Would you like to add another room? y/n");
+            input.nextLine();
+            addRoom = input.nextLine().toLowerCase().trim();
+        }
+    }
+}
--- src/UI/MainMenu.java
+++ src/UI/MainMenu.java
@@ -1,16 +1,18 @@
 package UI;
 
 import api.HotelResource;
-import com.sun.tools.javac.Main;
 
+
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class MainMenu {
-    public static void main(String[] args) {
+    public void start() {
         boolean keepRunning = true;
         try (Scanner scanner = new Scanner(System.in)) {
             while (keepRunning) {
                 try {
+                    System.out.println("MAIN MENU");
                     System.out.println("1. Find and Reserve a room");
                     System.out.println("2. See my reservations");
                     System.out.println("3. Create an account");
@@ -21,13 +23,27 @@ public class MainMenu {
                     int selection = Integer.parseInt(scanner.nextLine());
 
                     if (selection == 1) {
-                        System.out.println("1");
+//                        System.out.println("Enter email");
+//                        String customerEmail = scanner.nextLine().trim();
+//
+//                        System.out.println("Choose a room type");
+//                        IRoom room = scanner.nextLine().trim();
+//
+//                        System.out.println("Check in date");
+//                        Date checkInDate = scanner.nextLine().trim();
+//
+//                        System.out.println("Check out date");
+//                        Date checkOutDate = scanner.nextLine().trim();
+//
+//                        HotelResource.getInstance().bookARoom(customerEmail, room, checkInDate, checkOutDate);
                     }
                     if (selection == 2 ) {
-                        System.out.println("2");
+                        System.out.println("Enter email to see reservations");
+                        String customerEmail = scanner.next().trim();
+
+                        HotelResource.getInstance().getCustomerReservations(customerEmail);
                     }
                     if (selection == 3) {
-
                         System.out.println("Enter first name");
                         String firstName = scanner.nextLine();
 
@@ -41,7 +57,8 @@ public class MainMenu {
                         System.out.println("Customer created successfully");
                     }
                     if (selection == 4) {
-                        System.out.println("4");
+                        AdminMenu adminMenu = new AdminMenu();
+                        adminMenu.start();
                     }
                     if (selection == 5) {
                         System.out.println("5");
@@ -56,5 +73,8 @@ public class MainMenu {
         }
     }
 
-
+    public static void main(String[] args) {
+        MainMenu mainMenu = new MainMenu();
+        mainMenu.start();
+    }
 }
